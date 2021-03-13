66f8403705c0aacc4d02acbf4bb5b8c559e7abd2
Anas Altair <anastr244@gmail.com>
2021-03-13 13:31:31 +0200

    change degree format, add test for Utils

--- app/src/main/java/com/github/anastr/myscore/util/ExtUtils.kt
+++ app/src/main/java/com/github/anastr/myscore/util/ExtUtils.kt
@@ -1,8 +1,13 @@
 package com.github.anastr.myscore.util
 
+import java.text.DecimalFormat
+import java.text.DecimalFormatSymbols
 import java.util.*
 
-fun Float.formattedScore() = String.format(Locale.ENGLISH, "%.2f", this)
+fun Float.formattedScore(): String = DecimalFormat(
+    "#.##",
+    DecimalFormatSymbols(Locale.ENGLISH)
+).format(this)
 
 fun <T> MutableList<T>.swap(i1: Int, i2: Int) {
 //    this[i1] = this[i2].also { this[i2] = this[i1] }
---
+++ app/src/test/java/com/github/anastr/myscore/util/UtilsKtTest.kt
@@ -0,0 +1,32 @@
+package com.github.anastr.myscore.util
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class UtilsKtTest {
+
+    @Test
+    fun testSwap() {
+        val list = listOf("First", "Second")
+        val mutableList = list.toMutableList()
+        mutableList.swap(0, 1)
+        assertEquals(list[0], mutableList[1])
+        assertEquals(list[1], mutableList[0])
+    }
+
+    @Test
+    fun testDegreeFormatter() {
+        assertEquals("90", 90f.formattedScore())
+        assertEquals("90", 90.0f.formattedScore())
+        assertEquals("90.1", 90.1f.formattedScore())
+        assertEquals("90.7", 90.7f.formattedScore())
+        assertEquals("90.12", 90.12f.formattedScore())
+        assertEquals("90.12", 90.125f.formattedScore())
+        assertEquals("90.13", 90.127f.formattedScore())
+    }
+
+    @Test
+    fun testYearsResList() {
+        assertEquals(yearsRec.size, MAX_YEARS)
+    }
+}
