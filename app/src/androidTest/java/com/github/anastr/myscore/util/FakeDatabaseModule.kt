67506faa9dc5c26490df693b0a5cf444d313ec5d
Anas Altair <anastr244@gmail.com>
2021-05-27 06:01:25 +0300

    Add Hilt to unit test

---
+++ app/src/androidTest/java/com/github/anastr/myscore/util/FakeDatabaseModule.kt
@@ -0,0 +1,25 @@
+package com.github.anastr.myscore.util
+
+import android.content.Context
+import androidx.room.Room
+import com.github.anastr.myscore.hilt.DatabaseModule
+import com.github.anastr.myscore.room.AppDatabase
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import dagger.hilt.testing.TestInstallIn
+import javax.inject.Singleton
+
+@Module
+@TestInstallIn(
+    components = [SingletonComponent::class],
+    replaces = [DatabaseModule::class],
+)
+class FakeDatabaseModule {
+    @Singleton
+    @Provides
+    fun provideAppDatabase(@ApplicationContext context: Context): AppDatabase {
+        return Room.inMemoryDatabaseBuilder(context, AppDatabase::class.java).build()
+    }
+}
