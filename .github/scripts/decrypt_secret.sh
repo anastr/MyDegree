701f879d34a17b2951c9d335c0770074c9a773ab
Anas Altair <anastr244@gmail.com>
2021-09-11 11:29:35 +0300

    Update sh.

--- .github/scripts/decrypt_secret.sh
+++ .github/scripts/decrypt_secret.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 
-# Decrypt the file
 mkdir $HOME/app
-# --batch to prevent interactive command
-# --yes to assume "yes" for questions
+
 gpg --quiet --batch --yes --decrypt --passphrase="$GOOGLE_JSON_SECRET_PASSPHRASE" \
---output $HOME/app/google-services.json google-services.json.gpg
+--output $HOME/app/google-services.json $HOME/.github/scripts/google-services.json.gpg
